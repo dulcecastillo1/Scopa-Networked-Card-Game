d8837c69cddec1f3206b6874718c6577a2c2b6d7
Dulce Castillo <dulcecastillopb@gmail.com>
2025-10-18 14:07:32 -0500

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,38 @@
+import java.io.Serializable; 
+public class Node<E> implements Serializable{
+    
+    private E data; 
+    private Node<E> next; 
+    private Node<E> prev;  
+
+    public Node(E data){
+        this.data = data;
+        this.next = null; 
+        this.prev = null;  
+    }
+
+    public E get(){
+        return data; 
+    }
+
+    public Node<E> next(){
+        return next; 
+    }
+
+    public Node<E> prev(){
+        return prev; 
+    }
+
+    public void setNext(Node<E> next){
+        this.next = next; 
+    }
+
+    public void setPrev(Node<E> prev){
+        this.prev = prev; 
+    }
+
+    public void set(E data){
+        this.data = data; 
+    }
+
+}
